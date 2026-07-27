8fbf2c8bc1030d49d70714680d2346ea0f8c24c4
Ahsaan <164712814+ahsaan99@users.noreply.github.com>
2026-07-27 11:53:17 +0400

    Update hio.py

--- hio.py
+++ hio.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import time
  
-# Direct imports to bypass audio hangs
+# Direct imports to bypass audio hangsS 
 from mediapipe.python.solutions import hands as mp_hands
 from mediapipe.python.solutions import drawing_utils as mp_draw
 
